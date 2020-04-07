646822cd68b98e3f3b93704dd05bcddd6f9aa964
DazWilkin <daz.wilkin@gmail.com>
2020-04-07 13:49:07 -0700

    Refactored googleapis into rust-googleapis

--- build.rs
+++ build.rs
@@ -1,76 +1,33 @@
+use git2::Repository;
+
+use std::path::Path;
+
 fn main() {
-    // stepancheg/rust-protobuf -- recommended
-    // protoc_rust::run(protoc_rust::Args {
-    //     out_dir: "src/protos",
-    //     includes: &[
-    //         "/home/dazwilkin/Projects/trillian",
-    //         "/home/dazwilkin/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.9.4/third_party/googleapis",
-    //     ],
-    //     input: &["/home/dazwilkin/Projects/trillian/trillian_log_api.proto",],
-    //     customize: protoc_rust::Customize {
-    //         ..Default::default()
-    //     },
-    // })
-    // .expect("protoc");
-
-    // protoc_rust::run(protoc_rust::Args {
-    //     out_dir: "src/protos",
-    //     includes: &["googleapis"],
-    //     input: &["protos/greeter.proto"],
-    //     customize: protoc_rust::Customize {
-    //         ..Default::default()
-    //     },
-    // })
-    // .expect("protoc");
-
-    // stepancheg/rust-protobuf -- alpha
-    // protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-    //     out_dir: "./src/protos",
-    //     includes: &[
-    //         "/home/dazwilkin/Projects/rust-trillian/trillian/",
-    //         "/home/dazwilkin/Projects/rust-trillian/grpc-gateway/third_party/googleapis/",
-    //     ],
-    //     input: &["trillian_log_api.proto"],
-    //     customize: protobuf_codegen_pure::Customize {
-    //         ..Default::default()
-    //     },
-    // })
-    // .expect("protoc");
-
-    // protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-    //     out_dir: "./src/protos",
-    //     includes: &["protos"],
-    //     input: &["greeter.proto"],
-    //     customize: protobuf_codegen_pure::Customize {
-    //         ..Default::default()
-    //     },
-    // })
-    // .expect("protoc");
+    Repository::clone(
+        "https://github.com/googleapis/googleapis.git",
+        Path::new("googleapis"),
+    )
+    .expect("failed to clone");
+    Repository::clone(
+        "https://github.com/google/trillian.git",
+        Path::new("trillian"),
+    )
+    .expect("failed to clone");
 
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src/protos",
-        includes: &[
-            "/home/dazwilkin/Projects/trillian",
-            "/home/dazwilkin/go/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.9.4/third_party/googleapis",
-        ],
+        out_dir: "src/google",
+        includes: &["googleapis", "trillian"],
         input: &[
-            "/home/dazwilkin/Projects/trillian/trillian_admin_api.proto",
-            "/home/dazwilkin/Projects/trillian/trillian_log_api.proto",
-            "/home/dazwilkin/Projects/trillian/trillian_log_sequencer_api.proto",
-            "/home/dazwilkin/Projects/trillian/trillian_map_api.proto",
-            "/home/dazwilkin/Projects/trillian/trillian.proto",
-            ],
-        rust_protobuf: true, // also generate protobuf messages, not just services
+            "trillian/trillian_admin_api.proto",
+            "trillian/trillian_log_api.proto",
+            "trillian/trillian_log_sequencer_api.proto",
+            "trillian/trillian_map_api.proto",
+            "trillian/trillian.proto",
+            "trillian/crypto/keyspb/keyspb.proto",
+            "trillian/crypto/sigpb/sigpb.proto",
+        ],
+        rust_protobuf: true,
         ..Default::default()
     })
     .expect("protoc");
-
-    // protoc_rust_grpc::run(protoc_rust_grpc::Args {
-    //     out_dir: "src/protos",
-    //     includes: &["protos"],
-    //     input: &["greeter.proto"],
-    //     rust_protobuf: true, // also generate protobuf messages, not just services
-    //     ..Default::default()
-    // })
-    // .expect("protoc");
 }
---
+++ src/google/mod.rs
@@ -0,0 +1,18 @@
+pub use rust_googleapis::google::rpc::status;
+
+mod keyspb;
+mod sigpb;
+
+pub mod trillian;
+
+pub mod trillian_admin_api;
+pub mod trillian_admin_api_grpc;
+
+pub mod trillian_log_api;
+pub mod trillian_log_api_grpc;
+
+pub mod trillian_log_sequencer_api;
+pub mod trillian_log_sequencer_api_grpc;
+
+pub mod trillian_map_api;
+pub mod trillian_map_api_grpc;
